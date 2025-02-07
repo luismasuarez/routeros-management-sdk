68b86306f96165c3397a412ca40f9543fdcf954d
Luisma <luismasuarezzz@gmail.com>
2025-02-07 11:16:52 -0500

    remove stupid comment

--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,6 @@ import { TLSSocket } from 'tls';
 
 export type TSocket = Socket | TLSSocket | null
 
-/**
-* Tipo que representa un objeto con pares clave-valor.
-*/
 export type ParsedObject = { [key: string]: string | null };
 
 export type RouterOSResponse = { type: string; attributes: Record<string, string> };
